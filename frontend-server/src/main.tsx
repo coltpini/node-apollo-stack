e351daaa03f2d9fc934c7ed890df29d2f6d58153
Colt Pini <coltpini@gmail.com>
2024-08-22 12:27:37 -0600

    adding FE

---
+++ frontend-server/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./components/container/App/App";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
